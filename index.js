7a3a2b57e059bce6943a8b6d9457fce59d32488e
sophiacrispy <sophiacrispy1@gmail.com>
2024-04-14 03:35:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,160 @@
+// sntax
+
+//console.log("Hello World")
+// variables are containers use to stay code
+// type of variables.
+//var, let, const, what is the difference between this variables
+//let x = 5
+//let y = 10
+//let country = "nigeria"
+
+//console.log(x)
+//console.log(y)
+//console.log(country)
+
+// Data tyes
+// data types are type of values javascript can manipulate e.g data type, javascript have some key words you are not expected to use of strings is represented with double quote, strings are bounch of characters.
+//let names = "john 12345688376"// string
+
+//console.log(names)
+
+// what are javascript key words.
+// integers arfe numbers.
+//what are the types of data types.
+//boolen is a data type that is asigned to data for a true or false.
+//const names = " rosee 2786348iy83"// string
+//const num =23 // integer
+//const isMale = false //boolean
+
+//console.log(names)
+//console.log(num)
+//console.log(isMale)
+
+//let y = 10
+//let x = 5
+//console.log is a method eg Date.day( eg 
+
+//const date = new Date() +1
+
+//console.log(date)
+//types of error in javascript
+//let firstName = "john"
+//let lastName = "peters"
+
+//let full_name =firstName +" "+ lastName
+
+//console.log(full_name)
+//let a = 5 //asigning a value 5 to a 
+//let b == 5 // b=5
+//let c === 5
+
+//const x = 5
+//const y = 5
+
+//if(x===y) { //if statement means that whatever you are looking for is true
+    //return console.log("its Truthy") //if statement will only run this statement when the condition is true.
+//}
+//else{
+    //return console.log("its false")
+//}
+//loops are structures that repeat a particlar kind of code untill instanceof
+ //a condition is Meant
+
+
+//a function in javascript design to perform a particular tasks. javascript function is excuted when something provokes instanceof. javscript is a block of code.
+//function myfnction(p1, p2) {
+     //const result = 2*3;
+
+     //console.log(result: "here is your result" + " " + result)
+
+//}
+//myfunction()
+
+//function students(firstname, lastname,etc) {
+
+    //const fulname = firstname + " " + lastname
+    //const ful_name = firstname + " " + lastname
+
+    //console.log("full name of the students " + " " + fulname)
+    //basic name convention for varibles
+    //1. camel case
+       // fullName, firstName, lastName
+
+       //2. snake case
+             //fll_name. church_assembly_area
+
+//function multiplyNumbers(numb1, numb2) {
+ //const result = numb1 * numb2;
+
+ //return result
+
+
+ //console.log(result: " here isyour result" + " " + result)
+            // }
+
+
+         //Array of object, is use to store multiple vaules in a single variables
+         
+         //const car = {
+            //name: "toyota",
+           ////
+           //color: "pink",
+           //// model: "classic230",
+         //}
+         // javascript array of methos as properties
+         //const cars = ["sabb", "volve", "BMW"];
+
+         //console.log (cars)
+
+         //Arrays method and properties
+         //concat, at, constructor
+        // Float() 2ArrayforEach()
+        //fill.
+        //const jss1a = ["blessing", "Volve", "BWM"];
+        //const jss1b = ["sweet", "love", "obi"];
+ 
+        //const jss1 = jss1a.concat(jss1b)
+
+        //console.log(jss1)
+
+        //const age1 = ["cellie", "lone"];
+        //const age2 = ["love", "tobias", "emil"];
+        //const age3 = ["robin"];
+
+        //const children = age1.concat(age1, age2);
+
+        //console.log(children)
+
+        //in conting in javascript, we start counting arrays in 0, 1 etc
+
+//const fruits = ["banana", "orange", "apple", "mango"];
+//console.log(fruits)
+
+//fruits.fill("kiwi",2, 4);
+
+//console.log(fruits)
+
+
+function dev( ){
+    console.log("dev")
+}
+// when you write a fuuunction yo have tto call the fnction for the function to be defined
+//a call back fnction gives the varible a nmber so that every thing will be install
+
+
+//call  back fuunction.
+
+const dev = () =>{
+
+}
+
+const dev = (part1, part2) => {
+    console.log("dev")
+
+}
+dev(2, 3)
+
+
+
+
+
